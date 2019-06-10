df96e093eb0cc3dfd24c7f3231e25c140357ea75
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-06-10 13:14:25 +1200

    Add project files.

---
+++ ImageAndSound/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ImageAndSound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PicBox_Main = new System.Windows.Forms.PictureBox();
+            this.Btn_First = new System.Windows.Forms.Button();
+            this.Btn_Second = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicBox_Main
+            // 
+            this.PicBox_Main.Location = new System.Drawing.Point(179, 12);
+            this.PicBox_Main.Name = "PicBox_Main";
+            this.PicBox_Main.Size = new System.Drawing.Size(374, 217);
+            this.PicBox_Main.TabIndex = 0;
+            this.PicBox_Main.TabStop = false;
+            // 
+            // Btn_First
+            // 
+            this.Btn_First.Location = new System.Drawing.Point(179, 304);
+            this.Btn_First.Name = "Btn_First";
+            this.Btn_First.Size = new System.Drawing.Size(75, 23);
+            this.Btn_First.TabIndex = 1;
+            this.Btn_First.Text = "Cricket";
+            this.Btn_First.UseVisualStyleBackColor = true;
+            this.Btn_First.Click += new System.EventHandler(this.Btn_First_Click);
+            // 
+            // Btn_Second
+            // 
+            this.Btn_Second.Location = new System.Drawing.Point(478, 304);
+            this.Btn_Second.Name = "Btn_Second";
+            this.Btn_Second.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Second.TabIndex = 2;
+            this.Btn_Second.Text = "Football";
+            this.Btn_Second.UseVisualStyleBackColor = true;
+            this.Btn_Second.Click += new System.EventHandler(this.Btn_Second_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Second);
+            this.Controls.Add(this.Btn_First);
+            this.Controls.Add(this.PicBox_Main);
+            this.Name = "Form1";
+            this.Text = "Sound and Image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Main)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicBox_Main;
+        private System.Windows.Forms.Button Btn_First;
+        private System.Windows.Forms.Button Btn_Second;
+    }
+}
+
---
+++ ImageAndSound/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Media;
+
+namespace ImageAndSound
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //code to display image in picture box on form load 
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+            Stream myStream = myAssembly.GetManifestResourceStream("ImageAndSound.Resources.sports.jpg");
+            Bitmap bmp = new Bitmap(myStream);
+            PicBox_Main.Image = bmp;
+        }
+
+
+        private void Btn_First_Click(object sender, EventArgs e)
+        {
+            //code to display image in picture box on button click 
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+            Stream myStream = myAssembly.GetManifestResourceStream("ImageAndSound.Resources.Cricket.jpg");
+            Bitmap bmp = new Bitmap(myStream);
+            PicBox_Main.Image = bmp;
+
+            //code to play sound on button click but before writting below code we need to folow the below mentioned steps
+            // 1. Open "Solution Explorer" from menu toolbar ("VIEW") or simply press Ctrl+Alt+L.
+            //2. Click on drop - down list of "Properties".
+            //3. Then select "Resource.resx" and press enter.
+            //4. click on string drop down (top left corner of the Resources.resx file) and select "Audio" from the combobox list.
+            //5. Then click on "Add Resource", choose audio files (.wav) and click "Open".
+            //6. Select audio file(s) and change "Persistence" properties to "Embedded in .resx".
+            //7. write below mentioned code
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(ImageAndSound.Properties.Resources.sound);
+            snd.Play();
+        }
+
+        private void Btn_Second_Click(object sender, EventArgs e)
+        {
+            //code to display image in picture box on button click 
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+            Stream myStream = myAssembly.GetManifestResourceStream("ImageAndSound.Resources.football.jpg");
+            Bitmap bmp = new Bitmap(myStream);
+            PicBox_Main.Image = bmp;
+
+            //code to play sound on button click but before writting below code we need to folow the below mentioned steps
+            // 1. Open "Solution Explorer" from menu toolbar ("VIEW") or simply press Ctrl+Alt+L.
+            //2. Click on drop - down list of "Properties".
+            //3. Then select "Resource.resx" and press enter.
+            //4. click on string drop down (top left corner of the Resources.resx file) and select "Audio" from the combobox list.
+            //5. Then click on "Add Resource", choose audio files (.wav) and click "Open".
+            //6. Select audio file(s) and change "Persistence" properties to "Embedded in .resx".
+            //7. write below mentioned code
+
+
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(ImageAndSound.Properties.Resources.gun);
+            snd.Play();
+        }
+    }
+}
